197c123a59c1910f3ca6f05dbe8aa16ac193a1cb
wkailiu <wenkailiu23@gmail.com>
2019-08-12 17:15:23 +0800

    Update main.cpp

--- openvino_test/main.cpp
+++ openvino_test/main.cpp
@@ -13,8 +13,49 @@ using namespace InferenceEngine;
 using namespace human_pose_estimation;
 using namespace std;
 
-int main(int argc, char* argv[]) {
 
+//提供给unity的接口
+#define DLL_API extern "C" __declspec (dllexport)
+
+DLL_API void initHumanPose3D();
+DLL_API void nextFrame(float* humanPoseData);
+
+
+CameraManager manager;
+HumanLib oldHumanLib, newHumanLib;
+
+
+
+void initHumanPose3D() {
+	StereoCamera stereo("stereo_zed.yaml");
+	manager.addStereoCamera(&stereo);
+}
+
+
+void nextFrame(float* humanPoseData) {
+	manager.readAllFrame();						//读取图像
+	//manager.undistort();						//畸变矫正
+	newHumanLib = manager.mergeMultiCamera();	//多视图融合
+	newHumanLib.merge(oldHumanLib);				//与上一帧融合
+
+	oldHumanLib = newHumanLib;					//保存为上一帧
+
+	int k = 0;
+	humanPoseData[0] = newHumanLib.getHumanNum();
+	k++;
+	for (auto human : newHumanLib.getHumanVector()) {
+		for (int node = 0; node < 18; node++) {
+			auto p = human.getCurrNodePoint(node);
+			humanPoseData[k++] = p(0);	//x
+			humanPoseData[k++] = p(1);	//y
+			humanPoseData[k++] = p(2);	//z
+		}
+	}
+}
+
+
+int main(int argc, char* argv[]) {
+	float humanPoseData[18 * 3 * 10 + 1];
 	glutInit(&argc, argv);
 
 	CameraManager manager;
@@ -26,22 +67,30 @@ int main(int argc, char* argv[]) {
 	FreeglutUI::setCameraView(*stereo.getLeftCameraPtr());
 
 	HumanLib oldHumanLib, newHumanLib;
-	double inferenceTime = 0.0;
-	int ttt = 0;
 	while (true) {
-		ttt++;
-		double t1 = static_cast<double>(cv::getTickCount());
 		manager.readAllFrame();						//读取图像
 		//manager.undistort();						//畸变矫正
 		newHumanLib = manager.mergeMultiCamera();	//多视图融合
 		newHumanLib.merge(oldHumanLib);				//与上一帧融合
+
 		FreeglutUI::showHuman(newHumanLib);			//绘画三维骨架图
+		
 		oldHumanLib = newHumanLib;					//保存为上一帧
-		if(ttt%10==0)
-			std::cout << "frameId=" << manager.getFrameId() << "\tframeRate=" << manager.getFrameRate() << std::endl;;
 		manager.imshow();
 		cv::waitKey(5);
 
+
+		int k = 0;
+		humanPoseData[0] = newHumanLib.getHumanNum();
+		k++;
+		for (auto human : newHumanLib.getHumanVector()) {
+			for (int node = 0; node < 18; node++) {
+				auto p = human.getCurrNodePoint(node);
+				humanPoseData[k++] = p(0);	//x
+				humanPoseData[k++] = p(1);	//y
+				humanPoseData[k++] = p(2);	//z
+			}
+		}
 		//double t2 = static_cast<double>(cv::getTickCount());
 		//if (inferenceTime == 0) {
 		//	inferenceTime = (t2 - t1) / cv::getTickFrequency() * 1000;
